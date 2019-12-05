c8c68b2634f0d87e5bb320aa080eacc6b777826b
dailysergey <guskov13sv@gmail.com>
2019-12-05 20:21:49 +0300

    finished abstract factory

--- AbstractFactory/Descr.md
+++ AbstractFactory/Descr.md
@@ -1,3 +1,5 @@
-﻿#Creational Pattern
+﻿# Creational Pattern
+
 Creates an instance of several families of classes
+
 ![Image of Yaktocat](https://www.dofactory.com/images/diagrams/net/abstract.gif)
--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -2,11 +2,128 @@
 
 namespace AbstractFactory
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HeroFactory orkFactory = new OrkFactory();
+            Hero ork = new Hero(orkFactory);
+            ork.Hit();
+            ork.Run();
+
+            HeroFactory voinFactory = new VoinFactory();
+            Hero voin = new Hero(voinFactory);
+            voin.Hit();
+            voin.Run();
+
+            Console.ReadLine();
+        }
+
+        abstract class Weapon
+        {
+            public abstract void Hit();
+        }
+
+
+        abstract class Movement
+        {
+            public abstract void Move();
+        }
+
+
+        class Arbalet : Weapon
+        {
+            public override void Hit()
+            {
+                Console.WriteLine("Pif-Paf");
+            }
+        }
+
+
+        class Sword : Weapon
+        {
+            public override void Hit()
+            {
+                Console.WriteLine("Hit with the sword");
+            }
+        }
+
+
+        class StepMovement : Movement
+        {
+            public override void Move()
+            {
+                Console.WriteLine("Steping");
+            }
+        }
+
+
+        class RunMovement : Movement
+        {
+            public override void Move()
+            {
+                Console.WriteLine("Running");
+            }
+        }
+
+
+        abstract class HeroFactory
+        {
+            public abstract Movement CreateMovement();
+            public abstract Weapon CreateWeapon();
+        }
+
+
+        class OrkFactory : HeroFactory
+        {
+            public override Movement CreateMovement()
+            {
+                return new StepMovement();
+            }
+
+            public override Weapon CreateWeapon()
+            {
+                return new Arbalet();
+            }
+        }
+
+
+        class VoinFactory : HeroFactory
+        {
+            public override Movement CreateMovement()
+            {
+                return new RunMovement();
+            }
+
+            public override Weapon CreateWeapon()
+            {
+                return new Sword();
+            }
+        }
+
+        //Client
+        //tip: class
+        class Hero
+        {
+            private Weapon weapon;
+            private Movement movement;
+
+            //tip: ctor
+            public Hero(HeroFactory factory)
+            {
+                weapon = factory.CreateWeapon();
+                movement = factory.CreateMovement();
+            }
+            public void Run()
+            {
+                movement.Move();
+            }
+            public void Hit()
+            {
+                weapon.Hit();
+            }
         }
     }
 }
+
