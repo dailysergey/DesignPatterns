673e58a7e082f3b2542ac33393573426df696958
dailysergey <ds@ru>
2019-12-06 16:55:03 +0300

    Added Adapter pattern

---
+++ Adapter/Descr.md
@@ -0,0 +1,29 @@
+﻿# Structural Pattern
+
+Match interfaces of different classes
+
+## Definition
+
+Convert the interface of a class into another interface clients expect. Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
+
+## Participants
+
+The classes and objects participating in this pattern are:
+
+### Target   (ChemicalCompound) 
+
+Defines the domain-specific interface that Client uses.
+
+### Adapter  (Compound) 
+
+Adapts the interface Adaptee to the Target interface.
+
+### Adaptee  (ChemicalDatabank) 
+
+Defines an existing interface that needs adapting.
+
+### Client  (AdapterApp) 
+
+Collaborates with objects conforming to the Target interface.
+
+![Facade](https://www.dofactory.com/images/diagrams/net/adapter.gif)
---
+++ Adapter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Driver driver = new Driver();
+            Auto auto = new Auto();
+            driver.Travel(auto);
+            Camel camel = new Camel();
+            // use adapter
+            ITransport camelTransport = new CamelToTransportAdapter(camel);
+            driver.Travel(camelTransport);
+
+            Console.Read();
+        }
+    }
+
+    interface ITransport
+    {
+        void Drive();
+    }
+    
+
+    class Auto : ITransport
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car drives");
+        }
+    }
+
+    class Driver
+    {
+        public void Travel(ITransport transport)
+        {
+            transport.Drive();
+        }
+    }
+    
+    interface IAnimal
+    {
+        void Move();
+    }
+
+    class Camel : IAnimal
+    {
+        public void Move()
+        {
+            Console.WriteLine("Camel walks along the dessert");
+        }
+    }
+
+    // Adapter from Camel to Transport
+    class CamelToTransportAdapter : ITransport
+    {
+        readonly Camel camel;
+        public CamelToTransportAdapter(Camel c)
+        {
+            camel = c;
+        }
+
+        public void Drive()
+        {
+            camel.Move();
+        }
+    }
+}
