d2ff2fd263a4504edbfb8598d73fdfeaf4e4c667
dailysergey <guskov13sv@gmail.com>
2019-12-06 07:25:37 +0300

    Attempt to realize Singleton pattern, need reafactoring

---
+++ Singleton/Descr.md
@@ -0,0 +1,9 @@
+﻿# Creational Pattern
+
+A class of which only a single instance can exist
+
+## Definition
+
+Ensure a class has only one instance and provide a global point of access to it.
+
+![Factory Method](https://www.dofactory.com/images/diagrams/net/singleton.gif)
---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer comp = new Computer();
+            comp.Launch("Windows 8.1");
+            Console.WriteLine(comp.OS.Name);
+
+            // у нас не получится изменить ОС, так как объект уже создан    
+            OS.Current.Name="Windows 10";
+            comp.OS = OS.Current;
+            Console.WriteLine(comp.OS.Name);
+
+            Console.ReadLine();
+        }
+    }
+    class Computer
+    {
+        public OS OS { get; set; }
+        public void Launch(string osName)
+        {
+            OS.Current.Name = osName;
+        }
+    }
+    class OS
+    {
+        private static readonly OS instance = new OS("");
+        public static OS Current => instance;
+        private OS(string name) {
+            Name = name;
+        }
+       // private static OS instance;
+
+        public string Name { get;  set; }
+
+    }
+}
