0d35cfce3a92d3ed5c8cf5f2ddc6cc7022eeca10
dailysergey <ds@ru>
2019-12-06 11:35:24 +0300

    Add popular structural pattern Facade

---
+++ Facade/Descr.md
@@ -0,0 +1,9 @@
+﻿# Structural Pattern
+
+A single class that represents an entire subsystem
+
+## Definition
+
+Provide a unified interface to a set of interfaces in a subsystem. Façade defines a higher-level interface that makes the subsystem easier to use.
+
+![Facade](https://www.dofactory.com/images/diagrams/net/facade.gif)
---
+++ Facade/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main()
+        {
+            TextEditor textEditor = new TextEditor();
+            Compiller compiller = new Compiller();
+            CLR clr = new CLR();
+
+            VisualStudioFacade ide = new VisualStudioFacade(textEditor, compiller, clr);
+
+            Programmer programmer = new Programmer();
+            programmer.CreateApplication(ide);
+
+            Console.Read();
+        }
+    }
+
+    /// <summary>
+    /// Text Editor Class with two methods:
+    /// <para>CreateCode()</para>
+    /// <para>Save()</para>
+    /// </summary>
+    class TextEditor
+    {
+        public void CreateCode()
+        {
+            Console.WriteLine("Writing code");
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saving...");
+        }
+    }
+
+    /// <summary>
+    /// Compiller Class with method:
+    /// <para>Compile()</para>
+    /// </summary>
+    class Compiller
+    {
+        public void Compile()
+        {
+            Console.WriteLine("Code compilation...");
+        }
+    }
+
+    /// <summary>
+    /// CLR Class with two methods:
+    /// <para>Execute()</para>
+    /// <para>Finish()</para>
+    /// </summary>
+    class CLR
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing app...");
+        }
+        public void Finish()
+        {
+            Console.WriteLine("Finished app.");
+        }
+    }
+
+    /// <summary>
+    /// VisualStudioFacade Class with two methods:
+    /// <para>Start</para>
+    /// <para>Stop</para>
+    /// </summary>
+    class VisualStudioFacade
+    {
+        readonly TextEditor textEditor;
+        readonly Compiller compiller;
+        readonly CLR clr;
+        public VisualStudioFacade(TextEditor te, Compiller compil, CLR clr)
+        {
+            this.textEditor = te;
+            this.compiller = compil;
+            this.clr = clr;
+        }
+
+        public void Start()
+        {
+            textEditor.CreateCode();
+            textEditor.Save();
+            compiller.Compile();
+            clr.Execute();
+        }
+
+        public void Stop()
+        {
+            clr.Finish();
+        }
+    }
+
+    class Programmer
+    {
+        public void CreateApplication(VisualStudioFacade facade)
+        {
+            facade.Start();
+            facade.Stop();
+        }
+    }
+}
