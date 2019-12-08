729f51870608d64422bd10984a4760ba333bda19
dailysergey <guskov13sv@gmail.com>
2019-12-08 22:43:48 +0300

    Continue with designing patterns. Observer pattern

---
+++ Observer/Descr.md
@@ -0,0 +1,34 @@
+﻿# Behavioral Pattern
+
+A way of notifying change to a number of classes
+
+## Definition
+
+Define a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically.Publisher-Subscriber (издатель-подписчик)
+
+## Participants
+
+The classes and objects participating in this pattern are:
+
+### Subject 
+
+* Knows its observers. Any number of Observer objects may observe a subject
+* Provides an interface for attaching and detaching Observer objects.
+
+
+### ConcreteSubject
+
+stores state of interest to ConcreteObserver
+sends a notification to its observers when its state changes
+
+### Observer
+
+Defines an updating interface for objects that should be notified of changes in a subject.
+
+### ConcreteObserver
+
+* Maintains a reference to a ConcreteSubject object
+* Stores state that should stay consistent with the subject's
+* Implements the Observer updating interface to keep its state consistent with the subject's
+
+![Observer](https://www.dofactory.com/images/diagrams/net/observer.gif)
---
+++ Observer/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stock stock = new Stock();
+            Bank bank = new Bank("American Express", stock);
+            Broker broker = new Broker("John Smith", stock);
+            // Imitation trading
+            stock.Market();
+            //Stop trading 
+            broker.StopTrade();
+            // Imitation trading
+            stock.Market();
+
+            Console.Read();
+        }
+    }
+
+    interface IObserver
+    {
+        void Update(Object ob);
+    }
+
+    interface IObservable
+    {
+        void RegisterObserver(IObserver o);
+        void RemoveObserver(IObserver o);
+        void NotifyObservers();
+    }
+
+    class Stock : IObservable
+    {
+        StockInfo sInfo; 
+
+        List<IObserver> observers;
+        public Stock()
+        {
+            observers = new List<IObserver>();
+            sInfo = new StockInfo();
+        }
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver o in observers)
+            {
+                o.Update(sInfo);
+            }
+        }
+
+        public void Market()
+        {
+            Random rnd = new Random();
+            sInfo.USD = rnd.Next(20, 40);
+            sInfo.Euro = rnd.Next(30, 50);
+            NotifyObservers();
+        }
+    }
+
+    class StockInfo
+    {
+        public int USD { get; set; }
+        public int Euro { get; set; }
+    }
+
+    class Broker : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+        public Broker(string name, IObservable obs)
+        {
+            this.Name = name;
+            stock = obs;
+            stock.RegisterObserver(this);
+        }
+        public void Update(object ob)
+        {
+            StockInfo sInfo = (StockInfo)ob;
+
+            if (sInfo.USD > 30)
+                Console.WriteLine($"{this.Name} broker sells Dollar;  Dollar: {sInfo.USD}");
+            else
+                Console.WriteLine($"{this.Name} broker buyes Dollar; Dollar: {sInfo.USD}");
+        }
+        public void StopTrade()
+        {
+            stock.RemoveObserver(this);
+            stock = null;
+        }
+    }
+
+    class Bank : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+        public Bank(string name, IObservable obs)
+        {
+            this.Name = name;
+            stock = obs;
+            stock.RegisterObserver(this);
+        }
+        public void Update(object ob)
+        {
+            StockInfo sInfo = (StockInfo)ob;
+
+            if (sInfo.Euro > 40)
+                Console.WriteLine($"{this.Name} bank sells Euro;  Euro: {sInfo.Euro}");
+            else
+                Console.WriteLine($"{this.Name} buyes Euro; Euro: {sInfo.Euro}");
+        }
+    }
+}
