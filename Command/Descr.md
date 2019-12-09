c1018715a0e8d2346e4c40e34023ec155cadb332
dailysergey <guskov13sv@gmail.com>
2019-12-09 07:09:15 +0300

    added Command pattern

---
+++ Command/Descr.md
@@ -0,0 +1,34 @@
+﻿# Behavioral Pattern
+
+Encapsulate a command request as an object
+
+## Definition
+
+Encapsulate a request as an object, thereby letting you parameterize clients with different requests, queue or log requests, and support undoable operations.
+
+## Participants
+
+The classes and objects participating in this pattern are:
+
+### Command 
+
+Declares an interface for executing an operation
+
+### ConcreteCommand
+
+Defines a binding between a Receiver object and an action
+Implements Execute by invoking the corresponding operation(s) on Receiver
+
+### Client
+
+Creates a ConcreteCommand object and sets its receiver
+
+### Invoker
+
+Asks the command to carry out the request
+
+### Receiver
+
+Knows how to perform the operations associated with carrying out the request.
+
+![Command](https://www.dofactory.com/images/diagrams/net/command.gif)
---
+++ Command/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pult pult = new Pult();
+            TV tv = new TV();
+            pult.SetCommand(new TVOnCommand(tv));
+            pult.PressButton();
+            pult.PressUndo();
+
+            Microwave microwave = new Microwave();
+            // 5000 - время нагрева пищи
+            pult.SetCommand(new MicrowaveCommand(microwave, 5000));
+            pult.PressButton();
+
+            Console.Read();
+        }
+    }
+
+    //Command
+    interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+
+    // Receiver - Получатель
+    class TV
+    {
+        public void On()
+        {
+            Console.WriteLine("Телевизор включен!");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Телевизор выключен...");
+        }
+    }
+
+    //ConcreteCommand
+    class TVOnCommand : ICommand
+    {
+        TV tv;
+        public TVOnCommand(TV tvSet)
+        {
+            tv = tvSet;
+        }
+        public void Execute()
+        {
+            tv.On();
+        }
+        public void Undo()
+        {
+            tv.Off();
+        }
+    }
+
+    // Invoker - инициатор
+    class Pult
+    {
+        ICommand command;
+
+        public Pult() { }
+
+        public void SetCommand(ICommand com)
+        {
+            command = com;
+        }
+
+        public void PressButton()
+        {
+            if(command != null)
+                command.Execute();
+        }
+        public void PressUndo()
+        {
+            if(command != null)
+                command.Undo();
+        }
+    }
+
+    //Reciever
+    class Microwave
+    {
+        public void StartCooking(int time)
+        {
+            Console.WriteLine("Подогреваем еду");
+            // имитация работы с помощью асинхронного метода Task.Delay
+            Task.Delay(time).GetAwaiter().GetResult();
+        }
+
+        public void StopCooking()
+        {
+            Console.WriteLine("Еда подогрета!");
+        }
+    }
+
+    //ConcreteCommand
+    class MicrowaveCommand : ICommand
+    {
+        Microwave microwave;
+        int time;
+        public MicrowaveCommand(Microwave m, int t)
+        {
+            microwave = m;
+            time = t;
+        }
+        public void Execute()
+        {
+            microwave.StartCooking(time);
+            microwave.StopCooking();
+        }
+
+        public void Undo()
+        {
+            microwave.StopCooking();
+        }
+    }
+}
